618c2722f189731f81533feb1f45f42ad361fe56
Irisu <myirisuchan@gmail.com>
2026-02-06 19:41:29 +0400

    feat: update compositor.window api

--- examples/window_manager/src/fusion.rs
+++ examples/window_manager/src/fusion.rs
@@ -15,37 +15,63 @@ wit_bindgen::generate!({
     world: "compositor",
 });
 
-static WINDOWS: Mutex<Vec<WindowId>> = Mutex::new(Vec::new());
+#[derive(Default)]
+struct GlobalState {
+    windows: Vec<WindowId>,
+}
 
-impl exports::fusion::compositor::wm_exports::Guest for crate::WindowManager {
-    fn new_toplevel(window: WindowId) {
-        WINDOWS.lock().unwrap().push(window);
-        Self::rearrange_windows();
+impl GlobalState {
+    #[must_use]
+    pub const fn new() -> Self {
+        Self { windows: vec![] }
     }
 
-    fn toplevel_destroyed(window: WindowId) {
-        WINDOWS.lock().unwrap().retain(|&w| w.inner != window.inner);
-        Self::rearrange_windows();
-    }
-
-    fn rearrange_windows() {
-        let windows = WINDOWS.lock().unwrap();
-        if windows.is_empty() {
+    pub fn rearrange_windows(&mut self) {
+        if self.windows.is_empty() {
             return;
         }
         let (screen_width, screen_height) = get_output_size();
-        let width_per_window = screen_width / windows.len() as u32;
+        let width_per_window = screen_width / self.windows.len() as u32;
 
-        for (i, window) in windows.iter().enumerate() {
+        for (i, window) in self.windows.iter().enumerate() {
             let window = *window;
             let x_pos = i as u32 * width_per_window;
             let y_pos = 0;
 
-            set_window_pos(window, x_pos, y_pos);
             set_window_size(window, width_per_window, screen_height);
             send_configure(window);
+            set_window_pos(window, x_pos, y_pos);
         }
     }
+}
+
+static STATE: Mutex<GlobalState> = Mutex::new(GlobalState::new());
+
+fn state<R>(f: impl FnOnce(&mut GlobalState) -> R) -> R {
+    let mut wm = STATE.lock().unwrap();
+    f(&mut wm)
+}
+
+impl exports::fusion::compositor::wm_exports::Guest for crate::WindowManager {
+    fn new_toplevel(window: WindowId) {
+        state(|wm| {
+            wm.windows.push(window);
+            wm.rearrange_windows();
+        });
+    }
+
+    fn toplevel_destroyed(window: WindowId) {
+        state(|wm| {
+            wm.windows.retain(|&w| w.inner != window.inner);
+            wm.rearrange_windows();
+        });
+    }
+
+    fn rearrange_windows() {
+        state(GlobalState::rearrange_windows);
+    }
+
+    fn on_commit(_: WindowId) {}
 }
 
 impl Guest for crate::WindowManager {
